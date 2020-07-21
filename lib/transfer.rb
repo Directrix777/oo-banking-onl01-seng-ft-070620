335db161d72ace9564e8ad8c097ebde43f575b74
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-21 17:28:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,6 +26,10 @@ class Transfer
   end
 
   def reverse_transfer
+    if @status == "complete"
+      @sender.deposit(@amount)
+      @receiver.balance = @receiver.balance - amount
+    end
   end
 
 end
