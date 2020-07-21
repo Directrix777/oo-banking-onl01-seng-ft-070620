c1889aac8f6a0b9b750408becda70a7cf88ca9d5
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-21 17:26:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,8 +20,8 @@ class Transfer
       @sender.balance = @sender.balance - amount
       @status = "complete"
     else
-      "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 
