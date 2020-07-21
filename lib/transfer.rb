6c8217d7a27f688b7b10dc41014934322775dc3c
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-21 17:13:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,19 @@
 class Transfer
 
-  def initialize
+  attr_reader :sender, :reciever, :amount
+  attr_accessor :status
+
+  def initialize(sender, reciever, amount)
+    @sender = sender
+    @reciever = reciever
+    @status = "pending"
+    @amount = amount
   end
+
+  
+
+  
+
+  
   
 end
